8c8a36e81e4bda3fe34083e5682761494625264f
Student <b.nurdaulet.b@gmail.com>
2015-06-24 15:08:21 +0600

    Initial Commit

---
+++ day5/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  day5
+//
+//  Created by Student on 24.06.15.
+//  Copyright (c) 2015 KBTU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ day5/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  day5
+//
+//  Created by Student on 24.06.15.
+//  Copyright (c) 2015 KBTU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
